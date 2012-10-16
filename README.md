8fe0190534bb71811a12d0c4b18693c15aadd448
vagmi <vagmi.mudumbai@gmail.com>
2012-10-17 00:02:51 +0530

    initial commit - create project scaffold

---
+++ README.md
@@ -0,0 +1,3 @@
+# EarnTime
+
+[Earntime](http://earntime.in) helps you to track time based on activities and automatically generate beautiful invoices that will help you get paid on time. It will also help you budget your time and ensure that you are meeting the budget both as an individual and as a team.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Earntime::Application.config.secret_token = 'f7f7ab2ebe407b3e1d30aad6206f75433fbbc5982155059bd332abbcd9edca28b69c6ede0a58b3e1fa1c93973c8fcf2e99207d0b39040fd0d01ff4232413d3d1'
